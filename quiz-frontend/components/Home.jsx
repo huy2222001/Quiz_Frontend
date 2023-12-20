460e43ce52bcf5f34ba19af7b7d37ad2faca6b6b
tunghuy22t2 <nguyentunghuy02@gmail.com>
2023-12-20 22:04:16 +0700

    Get all quizs

---
+++ quiz-frontend/components/Home.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from "react"
+
+const Home = () => {
+	return (
+		<main>
+			<h2 className="mt-5">Welcome to online quiz for everyone</h2>
+		</main>
+	)
+}
+
+export default Home
