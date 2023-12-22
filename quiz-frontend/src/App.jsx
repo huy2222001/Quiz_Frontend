cf15bdb5d31ed4526ddf456fa4ea6aaa81248306
tunghuy22t2 <nguyentunghuy02@gmail.com>
2023-12-22 19:13:06 +0700

    Show Quiz result

--- quiz-frontend/src/App.jsx
+++ quiz-frontend/src/App.jsx
@@ -7,6 +7,9 @@ import Navbar from "../components/layout/NavBar.jsx"
 import AddQuestion from "./components/question/AddQuestion"
 import Admin from "../components/Admin.jsx"
 import UpdateQuestion from "../components/question/UpdateQuestion.jsx"
+import QuizResult from "../components/quiz/QuizResult.jsx"
+import GetAllQuiz from "../components/quiz/GetAllQuiz.jsx"
+import Quiz from "../components/quiz/Quiz.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,10 +20,13 @@ function App() {
 			<Navbar />
 				<Routes>
 					<Route path="/" element={<Home />} />
+					<Route path="/take-quiz" element={<Quiz />} />
 					<Route path="/admin" element={<Admin />} />
 
 					<Route path="/create-quiz" element={<AddQuestion />} />
 					<Route path="/update-quiz/:id" element={<UpdateQuestion />} />
+					<Route path="/all-quizzes" element={<GetAllQuiz />} />
+					<Route path="/quiz-result" element={<QuizResult />} />
 				</Routes>
 			</Router>
 		</main>
