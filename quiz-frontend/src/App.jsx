8b7afd3b9b4e2ec2c9e9339ce410c2a3b52e221b
tunghuy22t2 <nguyentunghuy02@gmail.com>
2023-12-22 10:39:14 +0700

    Recommit

--- quiz-frontend/src/App.jsx
+++ quiz-frontend/src/App.jsx
@@ -4,6 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "../components/Home.jsx"
 import Navbar from "../components/layout/NavBar.jsx"
+import AddQuestion from "./components/question/AddQuestion"
+import Admin from "../components/Admin.jsx"
+import UpdateQuestion from "../components/question/UpdateQuestion.jsx"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +17,10 @@ function App() {
 			<Navbar />
 				<Routes>
 					<Route path="/" element={<Home />} />
+					<Route path="/admin" element={<Admin />} />
+
+					<Route path="/create-quiz" element={<AddQuestion />} />
+					<Route path="/update-quiz/:id" element={<UpdateQuestion />} />
 				</Routes>
 			</Router>
 		</main>
